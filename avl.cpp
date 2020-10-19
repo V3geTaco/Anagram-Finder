fecff406cecec999afc8b7716c86a445d4b68952
V3geTaco <tobywang21@outlook.com>
2020-10-19 22:00:27 +1100

    added some basic tree functions (unfinished)

--- avl.cpp
+++ avl.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
+#include <cctype>
+#include <stdlib.h>
+#include <string.h>
+
 #include "avl.h"
+
+struct Node {
+   int  data;
+   int  height;
+   Tree left, right;
+};
+
+struct Tree {
+   Node root;
+};
 
 using namespace std;
 
-void Avl::rightRotation() {
+Tree Avl::newTree(void) {
+   return NULL;
+}
+
+Tree Avl::rightRotation(Tree n) {
+   if (n != NULL || n->left == NULL) {
+      return n;
+   }
+
+   Tree tmp = n->left;
+   n->right = tmp->left;
+   tmp->left = n;
+
+   n->height = max(height(n->left), height(n->right));
+   tmp->height = max(height(tmp->left), height(tmp->right));
+
+   return tmp;
+}
+
+Tree Avl::leftRotation(Tree n) {
+    // similar to right rotation
+    // not finished
+    return NULL;
+}
+
+ void Avl::freeTree(Tree t) {
+    if (t != NULL) {
+       freeTree(t->left);
+       freeTree(t->right);
+       free(t);
+    }
+ }
+
+static int Avl::height() {
 
 }
 
-void Avl::leftRotation() {
-    
+static int Avl::max(int x, int y) {
+   return (x >= y) ? x : y;
 }
--- avl.h
+++ avl.h
@@ -1,12 +1,21 @@
 #ifndef AVL_H
 #define AVL_H
 
+typedef struct Node *Tree;
+
 class Avl {
     private:
+        static int height(Tree n);
+        static int max(int x, int y);
+    protected:
 
     public:
-        void rightRotation();
-        void leftRotation();
+        Tree newTree(void);
+        void rightRotation(Tree n);
+        void leftRotation(Tree n);
+        void freeTree(Tree t);
 };
+
+
 
 #endif
